a1a680dbdd1173010397a7b908773955b725c52f
Juliano-Pasa <julianodemellopasa@gmail.com>
2022-01-09 02:12:33 -0300

    file_manipulation module

---
+++ Tools/file_manipulation.py
@@ -0,0 +1,13 @@
+"""
+    This module is responsable for writing new data into the txt files
+"""
+
+import csv
+
+def write_to_file(filename, metrics):
+    
+    file_path = "Data/" + filename + ".csv"
+    
+    with open(file_path, "a", newline='') as f:
+        writer = csv.writer(f)
+        writer.writerow(metrics)
